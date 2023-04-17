8214f58e381655ce53f042db16b010b482ed1c60
sourabh <bhattsourabh20@gmail.com>
2023-04-17 21:11:34 +0530

    added pdf

--- app/src/main/java/com/example/concretemixcalculator/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/com/example/concretemixcalculator/ui/slideshow/SlideshowFragment.kt
@@ -4,9 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.concretemixcalculator.databinding.FragmentSlideshowBinding
 
 class SlideshowFragment : Fragment() {
@@ -22,17 +21,29 @@ class SlideshowFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val slideshowViewModel =
-            ViewModelProvider(this).get(SlideshowViewModel::class.java)
 
         _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textSlideshow
-        slideshowViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        showPdfFromAssets(getPdfNameFromAssets())
+
+        return binding.root
+    }
+
+    fun getPdfNameFromAssets(): String {
+        return "report.pdf"
+    }
+
+    private fun showPdfFromAssets(pdfName: String) {
+        binding.pdfView.fromAsset(pdfName)
+            .password(null) // if password protected, then write password
+            .defaultPage(0) // set the default page to open
+            .onPageError { page, _ ->
+                Toast.makeText(
+                    requireContext(),
+                    "Error at page: $page", Toast.LENGTH_LONG
+                ).show()
+            }
+            .load()
     }
 
     override fun onDestroyView() {
