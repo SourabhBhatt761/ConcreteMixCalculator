6428f4ab16f5acd50af46588f5b3ba0d4bb339c7
sourabh <bhattsourabh20@gmail.com>
2023-04-17 21:30:11 +0530

    final changes

--- app/src/main/java/com/example/concretemixcalculator/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/example/concretemixcalculator/ui/gallery/GalleryViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class GalleryViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "Nothing much to show here"
     }
     val text: LiveData<String> = _text
 }
